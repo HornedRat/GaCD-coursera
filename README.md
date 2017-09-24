2fd04a0f6206c63e806a009cdccea1cfcff2b812
HornedRat <wiatrak667@op.pl>
2017-09-24 14:10:38 +0200

    Uploading all the files to Github

---
+++ README.md
@@ -0,0 +1,25 @@
+# Wearable computing data analysis
+
+This is a project for the Coursera "Getting and Cleaning Data" course.
+The purpose of this project is to create a tidy dataset from raw data on wearable computing and summarise it.
+
+## Setting up the script
+
+To run the analysis, working directory must contain a folder named "UCI HAR Dataset". The folder must contain the unzipped data from [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+## How to use the script
+
+The user can run the script all at once. The script will create two files in the working directory:
+* dataset.txt - a tidy dataset, containing the means and standard deviations for each measurement
+* summary.txt - a mean for every measurement for each subject and activity
+
+## What does the script do
+
+1. The script reads the raw data files
+2. It appends the subject numbers and activities to the datasets
+3. Merges the train and test data
+4. Gives the merged data proper column names
+5. Recodes the activities as a factor with proper levels
+6. Selects the columns of interest: mean and standard deviation of all measurements
+7. Creates a summary: mean of all the selected measurements for each subjects and activity
+8. Writes the dataset and summary to working directory
---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(data.table)
+
+#reads data into separate objects
+as.data.table(fread("./UCI HAR Dataset/train/X_train.txt")) -> train.data
+as.data.table(fread("./UCI HAR Dataset/train/y_train.txt")) -> train.activities
+as.data.table(fread("./UCI HAR Dataset/train/subject_train.txt")) -> train.subjects
+as.data.table(fread("./UCI HAR Dataset/test/X_test.txt")) -> test.data
+as.data.table(fread("./UCI HAR Dataset/test/y_test.txt")) -> test.activities
+as.data.table(fread("./UCI HAR Dataset/test/subject_test.txt")) -> test.subjects
+
+read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = F) -> var.labels
+read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors = F) -> activity.labels
+
+#merges the data into separate data tables for train and test, 
+train.dt <- cbind(train.subjects, train.activities, train.data)
+test.dt <- cbind(test.subjects, test.activities, test.data)
+
+#merging into one data table
+all.dt <- rbind(train.dt, test.dt)
+
+#giving proper names to the columns
+names(all.dt) <- c("subject", "activity", var.labels[,2])
+
+#recodes the activities as a factor with proper labels
+all.dt[,activity:=as.factor(activity)]
+levels(all.dt$activity) <- activity.labels[,2]
+
+#select the columns with mean() or std(), plus the subject and activity columns
+cols <- grep("mean\\()|std\\()", names(all.dt), ignore.case = T)
+dataset.dt <- all.dt[, c(1:2, cols), with = F]
+
+#removes the objects, that will no longer be used to free memory
+rm(activity.labels, test.activities, test.data, test.dt, test.subjects,
+   train.activities, train.data, train.dt, train.subjects, var.labels, cols, all.dt)
+
+#creates summary of all variables by subject and activity
+sub.acc.summary <- dataset.dt %>%
+    group_by(subject, activity) %>% summarise_all(mean)
+
+#writes the output tables to working directory
+write.table(dataset.dt, file = "dataset.txt", row.names = F)
+write.table(sub.acc.summary, file = "summary.txt", row.names = F)
